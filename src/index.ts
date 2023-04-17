d7e2051807fe0d941000a720783e161312ca10ab
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2023-04-17 21:15:23 +0300

    setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const PORT = Number(process.env.PORT ?? 80);
+const HOST = process.env.HOST ?? 'localhost';
+const SSL = process.env.SSL === 'true' || false;
+
+const app = express();
+
+app.get('/health', (req, res) => {
+    res.status(200).send("OK");
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`server ready at ${SSL ? 'https' : 'http'}://${HOST}:${PORT}`);
+})
