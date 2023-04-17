a08c5698757b46c4e0c843424f4d72ca2deba740
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2023-04-17 23:21:43 +0300

    remove comment

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,6 @@ app.get('/health', (req, res) => {
     res.status(200).send("OK");
 });
 
-//comment4
-
 app.listen(PORT, () => {
     console.log(`server ready at ${SSL ? 'https' : 'http'}://${HOST}`);
 })
