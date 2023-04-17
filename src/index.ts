ac88675b4d949b060570c7fab691228d58d54608
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2023-04-18 00:11:01 +0300

    websocket server

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,46 @@
+import http from 'http';
 import express from 'express';
+import WebSocket, {WebSocketServer} from 'ws';
+import {v4} from 'uuid';
+import { messageHandler } from './messageHandler';
+
 
 const PORT = Number(process.env.PORT ?? 80);
 const HOST = process.env.HOST ?? 'localhost';
 const SSL = process.env.SSL === 'true' || false;
 
-const app = express();
 
+
+
+const app = express();
+app.use(express.static('../public'));
 app.get('/', (req, res) => {
     res.status(200).send("OK");
 });
-
 app.get('/health', (req, res) => {
     res.status(200).send("OK");
 });
+const server = http.createServer(app);
 
-app.listen(PORT, () => {
-    console.log(`server ready at ${SSL ? 'https' : 'http'}://${HOST}`);
+const ws = new WebSocketServer({
+  server,
+  path: '/ws'
+});
+
+const connections = new Map<string, WebSocket>();
+
+ws.on('connection', (socket, req) => {
+    const connectionId = v4();
+    connections.set(connectionId, socket);
+    socket.on('message', (data, isBinary) => {
+        messageHandler(connections, connectionId, data, isBinary)
+    });
+    socket.on('close', () => {
+        connections.delete(connectionId);
+    })
+    socket.send(JSON.stringify({connectionId}));
 })
+
+server.listen(PORT, () => {
+    console.log(`server ready at ${SSL ? 'https' : 'http'}://${HOST}`);
+});
---
+++ src/messageHandler.ts
@@ -0,0 +1,5 @@
+import WebSocket from "ws";
+
+export async function messageHandler(connections: Map<string, WebSocket>, connectionId: string, data: any, isBinary: boolean) {
+
+}
