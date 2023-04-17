c5d7376c1271d9e1abee3a7d59f1d1289e58739f
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2023-04-18 00:29:40 +0300

    update script

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 (() => {
-    const ws = new WebSocket('/ws');
+    const ws = new WebSocket(`wss://${window.location.hostname}/ws`);
     ws.addEventListener('open', () => console.log('ws open'));
     ws.addEventListener('message', (e) => console.log(e.data));
 })()
