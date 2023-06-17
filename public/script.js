68930a724cca71b4745c917b9298da1ff302ddef
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2023-06-17 16:34:26 +0300

    nx monorepo

--- public/script.js
+++
@@ -1,5 +0,0 @@
-(() => {
-    const ws = new WebSocket(`wss://${window.location.hostname}/ws`);
-    ws.addEventListener('open', () => console.log('ws open'));
-    ws.addEventListener('message', (e) => console.log(e.data));
-})()
